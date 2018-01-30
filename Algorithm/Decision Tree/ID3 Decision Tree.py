34a0657e76233ca64e6ec36ce2fcd46606d8034b
Huangjianwen <1010445050@qq.com>
2018-01-30 22:30:21 +0800

    .

--- Algorithm/Decision Tree/ID3 Decision Tree.py
+++ Algorithm/Decision Tree/ID3 Decision Tree.py
@@ -116,6 +116,32 @@ def main():
     myTree=crateTree(dataSet,labels)
     storeTree(myTree,r'myTree.txt')
     print(grabTree(r'myTree.txt'))
+
+
+def lengthOfLongestSubstring( s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    if len(s) == 0: return 0
+    dict1 = {}
+    dict2 = {}
+    maxlength = 1
+    templength = 1
+    dict2[0] = s[0]
+    dict1[s[0]] = 0
+    for i in range(1, len(s)):
+        if s[i] not in dict2[i - 1]:
+            dict2[i] = dict2[i - 1] + s[i]
+            dict1[s[i]]=i
+            templength = len(dict2[i - 1]) + 1
+        else:
+            templength = i - dict1[s[i]]
+            dict2[i] = s[dict1[s[i]] + 1:i + 1]
+            dict1[s[i]] = i
+        maxlength = max(maxlength, templength)
+    return maxlength
 if __name__=='__main__':
-   main()
+   # main()
    #createLenses(r'F:\machinelearninginaction\Ch03\lenses.txt')
+   print(lengthOfLongestSubstring("pwke"))
